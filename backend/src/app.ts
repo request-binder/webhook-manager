851af0895884a9d2cd90a2748039735cca799575
Zach Morgan <zachmorgan97@gmail.com>
2023-05-22 18:12:04 -0700

    setup express

---
+++ backend/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+require('dotenv').config();
+
+const app: Application = express();
+const PORT = process.env.PORT;
+app.use(express.static('build')) // deployment
+app.use(express.json());
+
+
+app.get('/', (request: Request, response: Response, next: NextFunction) => {
+  response.send('Hello');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
